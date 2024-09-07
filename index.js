c1fbcfbd07bc05a91a54a3dbd366eff3b778d59a
Developer_Ramu <ramugupta808118@gmail.com>
2024-09-07 02:52:04 -0700

    serverless

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const cors = require("cors")
+// const cors = require("cors")
 const bodyParser = require('body-parser');
 require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 
