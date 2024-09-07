8540c27fecd9cf41479ea85e4a784ed88fa05f44
Developer_Ramu <ramugupta808118@gmail.com>
2024-09-07 02:35:36 -0700

    serverless

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.delete("/api/v1/books/:id", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT|| 1000;
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
